e4f419c19dccc24563b46ef292daffa04332998d
stefanbodoarca <stefanbodoarca@gmail.com>
2023-05-17 20:23:41 +0300

    create custom exception

---
+++ src/main/java/com/google/drive/api/exception/ListingFilesFromFolderException.java
@@ -0,0 +1,7 @@
+package com.google.drive.api.exception;
+
+public class ListingFilesFromFolderException extends RuntimeException {
+    public ListingFilesFromFolderException(String message) {
+
+    }
+}
